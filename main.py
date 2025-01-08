203ce178cc20f064d60a11aab8304e6be538cdd7
Sdg877 <sdrakegill@gmail.com>
2025-01-08 06:50:01 +0000

    Imports added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import asyncio
 
 from dotenv import load_dotenv
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli,  llm
+from livekit.agents.voice_assistant import VoiceAssistant
+
 load_dotenv()
 
